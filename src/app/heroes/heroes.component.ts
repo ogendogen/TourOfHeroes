3c8e1bbab74104779bf1fe177a840ce8b0b38905
ogendogen <kosmos500@interia.pl>
2024-07-23 20:57:03 +0200

    Update .gitignore

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.scss'
+})
+export class HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
