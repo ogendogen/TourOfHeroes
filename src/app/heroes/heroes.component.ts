e7747f0a46663dab49860fd40ce085efaa791337
ogendogen <kosmos500@interia.pl>
2024-07-25 21:00:58 +0200

    Messages service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,6 +4,7 @@ import { CommonModule, NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -16,7 +17,10 @@ export class HeroesComponent {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService,
+  ) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -24,13 +28,15 @@ export class HeroesComponent {
 
   onSelect(hero: Hero): void {
     if (hero == this.selectedHero) {
+      this.messageService.add(`HeroesComponent: Deselected hero id=${hero.id}`);
       this.selectedHero = undefined;
       return;
     }
     this.selectedHero = hero;
-  }
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);  }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
